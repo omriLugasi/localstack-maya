44c6986aea8e60fb57e6f77ed43a24a30f61ec51
omri <omlrc1991@gmail.com>
2023-05-13 10:47:20 +0300

    feat(s3 file page): align the s3 file page

--- webui/src/containers/S3/S3FilePage.tsx
+++ webui/src/containers/S3/S3FilePage.tsx
@@ -1,7 +1,15 @@
 import {useParams, useSearchParams } from "react-router-dom";
-import {useEffect} from "react";
+import {useEffect, useState} from "react";
 import {listObjectVersions} from "../../api/S3";
 import Button from "@mui/material/Button";
+import Paper from "@mui/material/Paper";
+import Table from "@mui/material/Table";
+import TableHead from "@mui/material/TableHead";
+import TableRow from "@mui/material/TableRow";
+import TableCell from "@mui/material/TableCell";
+import TableBody from "@mui/material/TableBody";
+import {Bucket} from "aws-sdk/clients/s3";
+import TableContainer from "@mui/material/TableContainer";
 
 interface IProps {
 }
@@ -9,26 +17,81 @@ interface IProps {
 export const S3FilePage = (props: IProps) => {
     const { bucketName } = useParams();
     const [searchParams] = useSearchParams();
+    const [versions, setVersions] = useState<{
+    IsLatest: boolean,
+    Key: string,
+    LastModified: Date,
+    Size: number
+    StorageClass: string
+    VersionId: string
+    }[]>([])
 
 
     const filePath = searchParams.get('path')
 
     useEffect(() => {
         const query = async () => {
-            listObjectVersions({
+            const response = await listObjectVersions({
                 Bucket: bucketName as string,
                 Prefix: filePath
-            }).then(console.log)
+            })
+            setVersions(response.Versions)
         }
 
         query()
     }, [])
 
     return(
-        <div>
-            <h1> Hello here ... {filePath} </h1>
-            <Button> Download </Button>
-            <Button> Open on new tab </Button>
+        <div className='s3-page-padding-left'>
+            <h1>S3 File page</h1>
+            <div className='sub-title-bar'>
+                <span>S3 file <a href="" className='link'>s3://{filePath}</a></span>
+                <div className='sub-title-bar-actions'>
+                    <Button
+                        data-qa='s3-upload-file-button'
+                        variant="contained"
+                        size='small'
+                    >Download file</Button>
+                </div>
+            </div>
+            <TableContainer component={Paper} style={{ width: '92%', margin: '2% 0'}}>
+                <Table>
+                    <TableHead>
+                        <TableRow>
+                            <TableCell>Version</TableCell>
+                            <TableCell>Latest</TableCell>
+                            <TableCell>Last Modified</TableCell>
+                            <TableCell>Size</TableCell>
+                        </TableRow>
+                    </TableHead>
+                    <TableBody>
+                        {versions.map((versionObject, index: number) => index > 7 ? null : (
+                            <TableRow
+                                key={versionObject.VersionId}
+                                sx={{ '&:last-child td, &:last-child th': { border: 0 } }}
+                            >
+                                <TableCell component="th" scope="row">
+                                <span
+                                    data-qa={`s3-bucket-column-name-${versionObject.VersionId}`}
+                                    className='link'
+                                >
+                                    {versionObject.VersionId === 'null' ? 'Not versioned' : versionObject.VersionId }
+                                </span>
+                                </TableCell>
+                                <TableCell>
+                                    { versionObject.IsLatest ? 'true' : 'false' }
+                                </TableCell>
+                                <TableCell>
+                                    {versionObject.LastModified.toISOString()}
+                                </TableCell>
+                                <TableCell>
+                                    {versionObject.Size}
+                                </TableCell>
+                            </TableRow>
+                        ))}
+                    </TableBody>
+                </Table>
+            </TableContainer>
         </div>
     )
 }
